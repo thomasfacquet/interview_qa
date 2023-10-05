dc1cd7adde9b60968ad54c64cdf9e2e37d50196b
Thomas facquet <thomas.facquet@follow.fr>
2023-10-05 12:00:04 +0200

    update test

--- cypress/e2e/advancedResearch.cy.js
+++ cypress/e2e/advancedResearch.cy.js
@@ -3,20 +3,18 @@
 context('Advanced research', () => {
   describe('Access to advanced research', () => {
     it('should display advanced research menu', () => {
+      cy.log('display advanced research');
       cy.visit('https://www.pokemon.com/fr/pokedex')
       cy.get("[role=alertdialog]").should("be.visible");
       cy.get("[id=onetrust-reject-all-handler]").should("be.visible").click();
       cy.findByText('Afficher la recherche avancée').should("be.visible").click();
       cy.get(".pokedex-filter-tw-list").should("be.visible");
-      cy
-      .get("[id=search]")
-      .should("be.visible")
-      .should("contain.text", "Rechercher");
-      cy.get("[id=reset]")
-      .should("be.visible")
-      .should("contain.text", "Réinitialiser");
-      
+      cy.get(".pokedex-filter-tw-list").find('li').should('have.length', 18)
+      cy.get("[id=minRangeBox]").should("be.visible").should("have.value", "1");
+      cy.get("[id=maxRangeBox]").should("be.visible").should("have.value", "1010");
+      cy.get("[id=advSearch]").should("be.visible")
+      cy.get("[id=reset]").should("be.visible")
+      cy.findByText('Cacher la recherche avancée').should("be.visible");
       })
   })
-
 })
