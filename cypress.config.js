5dd851367cde583e4b832261edd41faaf0f02161
Thomas facquet <thomas.facquet@follow.fr>
2023-10-05 09:28:47 +0200

    update config

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,21 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  watchForFileChanges: false,
+  screenshotsFolder: "output/screenshots",
+  videosFolder: "output/videos",
+  video: false,
+  viewportWidth: 1280,
+  viewportHeight: 800,
+  chromeWebSecurity: false,
+  pageLoadTimeout: 60000,
+  defaultCommandTimeout: 12000,
+  retries: {
+    runMode: 2,
+    openMode: 1,
+  },
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    chromeWebSecurity: true,
+    video: false,
   },
 });
--- cypress/e2e/advancedResearch.cy.js
+++ cypress/e2e/advancedResearch.cy.js
@@ -1,16 +1,21 @@
 /// <reference types="cypress" />
 
 context('Advanced research', () => {
-  describe('Acces to pokedex website', () => {
-    it('should access to the site', () => {
-      cy.visit('https://www.pokemon.com/fr/pokedex')
-      })
-  })
-
   describe('Access to advanced research', () => {
     it('should display advanced research menu', () => {
       cy.visit('https://www.pokemon.com/fr/pokedex')
+      cy.get("[role=alertdialog]").should("be.visible");
+      cy.get("[id=onetrust-reject-all-handler]").should("be.visible").click();
       cy.findByText('Afficher la recherche avancée').should("be.visible").click();
+      cy.get(".pokedex-filter-tw-list").should("be.visible");
+      cy
+      .get("[id=search]")
+      .should("be.visible")
+      .should("contain.text", "Rechercher");
+      cy.get("[id=reset]")
+      .should("be.visible")
+      .should("contain.text", "Réinitialiser");
+      
       })
   })
 
